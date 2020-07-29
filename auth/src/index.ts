8b189ef2e87b6724d723cd6b3e05a469008252c5
Kishore Newton <kishorenke@gmail.com>
2020-07-30 01:40:06 +0530

    added routes for signup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,16 @@
 import express from 'express'
 import { json } from 'body-parser'
+import { currentUserRouter } from './routes/current-user.router'
+import { signinRouter } from './routes/signin.router'
+import { signupRouter } from './routes/signup.router'
+import { signoutRouter } from './routes/signout.router'
 
 const app = express()
 app.use(json())
-
-app.get('/v1/api/users/currentuser', (req, res) => {
-  res.send('Hi There')
-})
+app.use(currentUserRouter)
+app.use(signinRouter)
+app.use(signoutRouter)
+app.use(signupRouter)
 
 app.listen(3000, () => {
   console.log('listening on port 3000')
